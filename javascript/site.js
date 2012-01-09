e329874d55f7e143f474c29fe1338ac32671ef39
Javier Burón <javierburon@colaborativa.eu>
2012-01-09 13:01:46 +0100

    Implementado hyphens en post main y primer boceto de la vista de cada post

--- javascript/site.js
+++ javascript/site.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-	check_position_and_layout();
-	$(".cuerpo li").each(function(i) {
-		var position = $(this).position();
+	var username='javiburon'; // set user name
+	var format='json';
+	var url='http://api.twitter.com/1/statuses/user_timeline/'+username+'.'+format+'?callback=?'; // make the url
+	$.getJSON(url,function(tweet){ // get the tweets
+		$("#ultimo_tweet").html(tweet[0].text); // get the first tweet in the response and place it inside the div
 	});
 });
-
-function check_position_and_layout(){
-	var anchura = $(".cuerpo").width();
-	console.log(anchura);
-}
